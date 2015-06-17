554a28da98bd497511dd4e97507bdaee3fa192f7
PrTesla <t.totota@gmail.com>
2015-06-17 15:37:14 +0200

    Scroll + Movement
    Scroll du BG
    Movment du player restreint dans la fenêtre de jeu.

--- FreeDaysGameJam/Assets/Scripts/PlayerMovement.cs
+++ FreeDaysGameJam/Assets/Scripts/PlayerMovement.cs
@@ -3,22 +3,41 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed;
+	public float speed = 500;
+
+	public float Xmin = -17f;
+	public float Xmax = 17f;
+	public float Ymin = -9.3f;
+	public float Ymax = 9.3f;
 
 	private Rigidbody2D _rigidbody;
-
-	// Use this for initialization
+	private Vector2 _velocity;
+	
 	void Start () 
 	{
 		_rigidbody = transform.GetComponent<Rigidbody2D>();
+		_velocity = Vector2.zero;
 	}
-	
-	// Update is called once per frame
+
 	void Update () 
 	{
-		Vector2 _velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		_velocity *= speed * Time.deltaTime;
+		//reset à chaque frame
+		_velocity = Vector2.zero;
 
+		if(Input.GetAxis("Horizontal") > 0 && transform.position.x < Xmax){
+			_velocity = new Vector2(Input.GetAxis("Horizontal"), _velocity.y);
+		} else if(Input.GetAxis("Horizontal") < 0 && transform.position.x > Xmin){
+			_velocity = new Vector2(Input.GetAxis("Horizontal"), _velocity.y);
+		}
+
+		if(Input.GetAxis("Vertical") > 0 && transform.position.y < Ymax){
+			_velocity = new Vector2(_velocity.x, Input.GetAxis("Vertical"));
+		} else if(Input.GetAxis("Vertical") < 0 && transform.position.y > Ymin){
+			_velocity = new Vector2(_velocity.x, Input.GetAxis("Vertical"));
+		}
+
+		_velocity *= speed * Time.deltaTime;
 		_rigidbody.velocity = _velocity;
+
 	}
 }
---
+++ FreeDaysGameJam/Assets/Scripts/Scroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	public float speed = 15;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float _Xadd = speed * Time.deltaTime;
+
+		transform.position += new Vector3(_Xadd, 0, 0);
+	}
+}
