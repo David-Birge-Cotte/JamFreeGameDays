b33760888f056cba7b600ca1517b19c4fb0903ed
PrTesla <t.totota@gmail.com>
2015-06-17 18:48:25 +0200

    Revert "Commit de Fin de journée"
    This reverts commit 7256b800a43733f8f29e65f1cd7f824b2c956ca2.

--- FreeDaysGameJam/Assets/Scripts/PlayerMovement.cs
+++ FreeDaysGameJam/Assets/Scripts/PlayerMovement.cs
@@ -10,26 +10,11 @@ public class PlayerMovement : MonoBehaviour {
 	public float Ymin = -9.3f;
 	public float Ymax = 9.3f;
 
-	public float YAttack = 5f;
-	public float YStopZone = -5f;
+	public float YAttack = -2f;
+
 
 	private Rigidbody2D _rigidbody;
 	private Vector2 _velocity;
-	private bool _isAttacking = false;
-	private bool _isGoingUp = false;
-	private float acceleration;
-
-	public float timerDown = 1f;
-
-	private enum PositionLayer{
-		UP,
-		MIDDLE,
-		DOWN,
-		ERROR
-	}
-
-	private PositionLayer pos;
-
 	
 	void Start () 
 	{
@@ -39,55 +24,12 @@ public class PlayerMovement : MonoBehaviour {
 
 	void Update () 
 	{
-		if(transform.position.y > YAttack)
-			pos = PositionLayer.UP;
-		else if(transform.position.y < YAttack && transform.position.y > YStopZone)
-			pos = PositionLayer.MIDDLE;
-		else if(transform.position.y < YStopZone)
-			pos = PositionLayer.DOWN;
-		else
-			pos = PositionLayer.ERROR;
+		_velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-		
-		if(Input.GetKey(KeyCode.Joystick1Button0) || pos == PositionLayer.MIDDLE)
+		if(Input.GetKey(KeyCode.Joystick1Button0) || transform.position.y < YAttack)
 		{
-			if(_isGoingUp == false)
-				_isAttacking = true;
+			Attack();
 		}
-
-		if(pos == PositionLayer.DOWN)
-		{
-			_isAttacking = false;
-			timerDown -= Time.deltaTime;
-		}
-
-		if(timerDown <= 0){
-			_isGoingUp = true;
-			timerDown = 1f;
-		}
-
-		if(pos == PositionLayer.UP){
-			_isGoingUp = false;
-		}
-
-		if(_isAttacking)
-		{
-			_velocity = new Vector2(0, -2);
-
-			acceleration += Time.deltaTime;
-			_velocity += new Vector2(0, -acceleration);
-		} else 
-		{
-			_velocity = Vector2.zero;
-			acceleration = 0;
-		}
-
-		if(_isGoingUp){
-			_velocity += new Vector2(0, 2);
-		}
-
-		_velocity += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
 
 		if(_velocity.x > 0 && transform.position.x >= Xmax)
 			_velocity.x = 0;
@@ -103,19 +45,25 @@ public class PlayerMovement : MonoBehaviour {
 		_velocity *= speed * Time.deltaTime;
 		_rigidbody.velocity = _velocity;
 
+		/*
+		#region ROTATION
+		Vector2 v = _rigidbody.velocity;
+		float angle = Mathf.Atan2(v.y, Mathf.Abs(v.x)) * Mathf.Rad2Deg;
+
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		#endregion
+		*/
 	}
+
+	void Attack()
+	{
+		_velocity += new Vector2(0, -2);
+	}
+
+
+
 }
 
-
-
-/*
-	#region ROTATION
-	Vector2 v = _rigidbody.velocity;
-	float angle = Mathf.Atan2(v.y, Mathf.Abs(v.x)) * Mathf.Rad2Deg;
-
-	transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-	#endregion
-*/
 
 
 
