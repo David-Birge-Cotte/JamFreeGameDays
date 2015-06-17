581d15bd9596f55e1542c007eadc7a2dd920a4cb
PrTesla <t.totota@gmail.com>
2015-06-17 14:41:44 +0200

    Alpha_01

---
+++ FreeDaysGameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D _rigidbody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_rigidbody = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 _velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		_velocity *= speed * Time.deltaTime;
+
+		_rigidbody.velocity = _velocity;
+	}
+}
