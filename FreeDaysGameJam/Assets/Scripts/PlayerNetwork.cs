7e4160c41e36b09b30c3d84d030068ebb69a21e4
PrTesla <t.totota@gmail.com>
2015-06-18 10:34:32 +0200

    Code custom networking v1

---
+++ FreeDaysGameJam/Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetwork : NetworkBehaviour {
+
+	[SyncVar]
+	private Vector3 syncPos;
+	
+	public float lerpRate = 15;
+	
+	void FixedUpdate()
+	{
+		TransmitPosition();
+		LerpPosition();
+	}
+	
+	void LerpPosition()
+	{
+		if(!isLocalPlayer)
+		{
+			transform.position = Vector3.Lerp(transform.position, syncPos, Time.deltaTime * lerpRate);
+		}
+	}
+
+	[Command]
+	void CmdProvidePositionToServer(Vector3 pos)
+	{
+		syncPos = pos;
+	}
+
+	[ClientCallback]
+	void TransmitPosition()
+	{
+		CmdProvidePositionToServer(transform.position);
+	}
+}
