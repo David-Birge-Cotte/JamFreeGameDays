04e40b0c15489be495dc218c567e7f7e5d13a33e
Raphael CARPENTIER <raphael.carpentier@gmail.com>
2015-06-18 09:15:30 +0200

    ajout comportement ennemy préliminaire

--- FreeDaysGameJam/Assets/Scripts/EnnemyBehaviour.cs
+++ FreeDaysGameJam/Assets/Scripts/EnnemyBehaviour.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class EnnemyBehaviour : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,14 @@ public class EnnemyBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+	void OnCollisionEnter2D(Collision2D coll ) 
+	{
+		if (coll.gameObject.tag == "Enemy")
+			coll.gameObject.SendMessage("ApplyDamage", 10);
+	}
+
+
 }
