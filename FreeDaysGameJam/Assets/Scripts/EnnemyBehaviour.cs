bd92b59b02841bded6063b18b835c2418de7be1d
Raphael CARPENTIER <raphael.carpentier@gmail.com>
2015-06-18 10:13:42 +0200

    Ajout test nombre de points

--- FreeDaysGameJam/Assets/Scripts/EnnemyBehaviour.cs
+++ FreeDaysGameJam/Assets/Scripts/EnnemyBehaviour.cs
@@ -3,7 +3,15 @@ using System.Collections;
 
 public class EnnemyBehaviour : MonoBehaviour {
 
+	public enum EnnemyType
+	{
+		plagiste,
+		poisson,
+		requin,
+		nageur
 
+	}
+	public EnnemyType ennemyType;
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +26,30 @@ public class EnnemyBehaviour : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll ) 
 	{
 		if (coll.gameObject.tag == "Player") {
-			coll.gameObject.SendMessage ("GetPoints", 10);
+			int pointsReceived = 0;
+
+			switch (ennemyType) {
+			case EnnemyType.plagiste: 
+				pointsReceived = 10;
+				break;
+			case EnnemyType.nageur: 
+				pointsReceived = 15;
+				break;
+			case EnnemyType.poisson: 
+				pointsReceived = 5;
+				break;
+			case EnnemyType.requin: 
+				pointsReceived = 20;
+				break;	
+			default:
+				break;
+			}
+			coll.gameObject.SendMessage ("GetPoints", pointsReceived);
 			Destroy(transform.gameObject);
 		}
+
+			
+
 	}
 
 }
