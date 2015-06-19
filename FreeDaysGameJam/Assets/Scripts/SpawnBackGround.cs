75f32e4dc313f3e40bdaa85aa231ee865facbd9c
Raphael CARPENTIER <raphael.carpentier@gmail.com>
2015-06-19 11:47:32 +0200

    ajout spawn background

--- FreeDaysGameJam/Assets/Scripts/SpawnBackGround.cs
+++ FreeDaysGameJam/Assets/Scripts/SpawnBackGround.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class SpawnBackGround : MonoBehaviour {
 
+	public GameObject Beach;
+	public GameObject Sea01;
+
+	bool LastBackGroundIsSea =true;
+	bool LastBackGroundIsbeach = false;
+
+	Vector3 pos;
+
 	// Use this for initialization
 	void Start () {
-	
+		 pos = new Vector3(transform.position.x, -10.7f, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+	void SpawnBackground ()
+	{
+
+
+		if(LastBackGroundIsSea)
+		{
+			Instantiate(Beach, pos, Quaternion.identity);
+			LastBackGroundIsSea =false;
+			LastBackGroundIsbeach = true;
+		}
+		else if(LastBackGroundIsbeach)
+		{
+			Instantiate(Sea01, pos, Quaternion.identity);
+			LastBackGroundIsSea =true;
+			LastBackGroundIsbeach = false;
+		}
+
+	}
+
 }
